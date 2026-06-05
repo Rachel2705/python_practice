94b730fb220ecf76de5c2e19181a13a36f2bccdc
Rachel2705 <rachelcasendra@gmail.com>
2026-06-05 19:30:27 +0530

    Add GC content and reverse complement functions

--- lesson1.py
+++ lesson1.py
@@ -1,5 +1,21 @@
 dna = 'ATGCGTACGTTAG'
 
+def calculate_gc_content(sequence):
+    g_count = sequence.count("G")
+    c_count = sequence.count("C")
+    gc_content = (g_count + c_count) / len(sequence) * 100
+    return gc_content
+
+def get_reverse_complement(sequence):
+    complement = sequence.replace("A", "t")
+    complement = complement.replace("T", "a")
+    complement = complement.replace("G", "c")
+    complement = complement.replace("C", "g")
+    complement = complement.upper()
+
+    reverse_complement = complement[::-1]
+    return reverse_complement
+
 length = len(dna)
 
 a_count = dna.count('A')
@@ -7,7 +23,7 @@ t_count = dna.count('T')
 g_count = dna.count('G')
 c_count = dna.count('C')
 
-gc_content = (g_count + c_count) / length * 100
+gc_content = calculate_gc_content(dna)
 
 print("DNA sequence:", dna)
 print("Length:", length)
@@ -16,3 +32,13 @@ print("T count:", t_count)
 print("G count:", g_count)
 print("C count:", c_count)
 print("GC content:", round(gc_content, 2), "%")
+
+complement = dna.replace("A", "t")
+complement = complement.replace("T", "a")
+complement = complement.replace("G", "c")
+complement = complement.replace("C", "g")
+complement = complement.upper()
+
+reverse_complement = complement[::-1]
+print("complement:", complement)
+print("Reverse complement:", reverse_complement)
